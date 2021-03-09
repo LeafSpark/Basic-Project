40ed00302c1ddb33f1efe574e3f83a7a222b162c
Tom-java-tech <78000825+Tom-java-tech@users.noreply.github.com>
2021-03-08 16:32:41 -0800

    Just the first basic hello world program file to expand.

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  
+  public void start() {
+    System.out.println("Hello World!");  //This is where it all begins.
+  }
+}
+
+public static void main(String args[]) {
+  Main mainStart = new Main();
+  mainStart.start();
+}
